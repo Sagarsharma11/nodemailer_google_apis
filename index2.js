6d128232a4434e7e0c828198ccf16c0e374937fd
Sagar Sharma <63957745+Sagarsharma11@users.noreply.github.com>
2023-04-12 00:50:19 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,10 +1,10 @@
 const nodemailer = require('nodemailer')
 const {google} = require('googleapis')
 
-const CLIENT_ID = '853949886656-a09ec33df0a9tr6h1vjdsn5rjqae1t9u.apps.googleusercontent.com'
-const CLIENT_SECRET = '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'
+const CLIENT_ID = '83df0a9tr6h1t9u.apps.googleusercontent.com'
+const CLIENT_SECRET = 'GOCSPX-XSU0fSvY4KDrmxwvq'
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
-const REFRESH_TOKEN = '1//04U9rdXrjqObLCgYIARAAGAQSNwF-L9IrVl5QIX74B755mfRdxo4O1WfTgIUxfY50klQCGxXNa1srarBZxiWdb5a10K7VZ09ATaY'
+const REFRESH_TOKEN = '1//04U9rdXrjqO1WfTgIUxfY50klQCGxXNa1srarBZxiWdb5a10K7VZ09ATaY'
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID,CLIENT_SECRET,REDIRECT_URI)
 
@@ -19,7 +19,7 @@ const sendMail = async ()=>{
                 service:'gmail',
                 auth:{
                     type:'OAuth2',
-                    user:'ss365493@gmail.com',
+                    user:'ss5493@gmail.com',
                     clientId:CLIENT_ID,
                     clientSecret: CLIENT_SECRET,
                     refreshToken: REFRESH_TOKEN,
@@ -28,8 +28,8 @@ const sendMail = async ()=>{
             }
         )
         const mailOption = {
-            from:'ss365493@gmail.com',
-            to:'ss365493@gmail.com',
+            from:'ss493@gmail.com',
+            to:'ss493@gmail.com',
             subject:'hello world',
             text:'this is hello world',
             html:'<h1 style="color:red"> Chu ho </h1>'
